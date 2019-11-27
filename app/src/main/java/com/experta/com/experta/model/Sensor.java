cae201fbe5dede5d1e22de2c14901831a04d55f3
Andrés Butelman <abutelman@gmail.com>
2019-11-27 20:39:50 -0300

    Recomendaciones de siniestros pasadas por el cliente

--- app/src/main/java/com/experta/com/experta/model/Sensor.java
+++ app/src/main/java/com/experta/com/experta/model/Sensor.java
@@ -2,9 +2,9 @@ package com.experta.com.experta.model;
 
 public enum Sensor {
 
-    CO("Se detectó que el MONÓXIDO DE CARBONO en tu ambiente está por encima del límite seguro, por lo tanto lorem ipsum..."),
-    GAS("Se detectó que el GAS NATURAL en tu ambiente está por encima del límite seguro, por lo tanto lorem ipsum..."),
-    SMOKE("Se detectó HUMO en tu ambiente, lorem ipsum");
+    CO("ALARMA DE CO. En forma urgente abra ventanas y puertas totalmente, evacue a todas las personas del lugar y llame al 911.\n"),
+    GAS("ALARMA DE GAS. No encienda ni apague la luz ni ningún otro dispositivo. Abra ventanas y puertas y cierre el flujo de gas desde la llave de paso. Evacue el lugar y llame al 911."),
+    SMOKE("ALARMA DE HUMO. Por favor evacúe el lugar lo antes posible y llame al 911.");
 
     private String recommendation;
 
